916d08c762f69642a66ee4ee324ae882d2c5ab66
angelsl003 <lasl11403@gmail.com>
2024-04-09 20:51:23 +0200

    Primera Consulta Hecha

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        //usuario:angel123
+        //pass:1234
+
     }
 }
